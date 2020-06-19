2aab0ce80683a04a5bf8e4fe53900506579a7523
squidnation7893 <jessedill123@gmail.com>
2020-06-19 01:46:55 -0400

    Movement In Progress

---
+++ Assets/Mover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    [SerializeField] Transform target;
+    Ray lastRay;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray,out hit); //hit = whatever the ray collides with in real world
+        if (hasHit)
+        {
+            navMeshAgent.SetDestination(hit.point);
+        }
+    }
+}
